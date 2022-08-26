96146c97d41ba987032496b3121a7c25863a994e
fadamos <hepworth.adam@gmail.com>
2022-08-26 18:09:15 +1000

    Create IntelligentDECISION.m

---
+++ IntelligentDECISION.m
@@ -0,0 +1,9 @@
+function output = IntelligentDECISION(Intent, Scenario, Tactic)
+    % Author: Adam J Hepworth
+    % LastModified: 2022-08-26
+    % Explanaton: Intelligent control agent   
+
+    if ~exist('Verbose', 'var')
+        Verbose=false; 
+    end
+end
