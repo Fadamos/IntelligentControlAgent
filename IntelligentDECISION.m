ed6938d7307d8ce154b4a534c07757aebf85a558
fadamos <hepworth.adam@gmail.com>
2022-08-28 17:37:09 +1000

    IntelligentDECISION

--- IntelligentDECISION.m
+++ IntelligentDECISION.m
@@ -1,21 +1,21 @@
 function output = IntelligentDECISION(df, Intent, Scenario)
     % Author: Adam J Hepworth
-    % LastModified: 2022-08-27
-    % Explanaton: Intelligent control agent library for a particular scenario and intent 
+    % LastModified: 2022-08-28
+    % Explanaton: Intelligent control agent data library 
 
     if ~exist('Scenario', 'var')
-        Scenario = "S0"; 
+        Scenario = "S0"; % if scenario is not given, default to best tactic-pair across all scenarios
+    end
+    
+    if Scenario == "S0"
+        subsetDF = df(:,[Intent 13:15]); 
+    else
+        subsetIndex = find(df.Scenario == Scenario);
+        subsetDF = df(subsetIndex,[Intent 13:15]);
     end
 
     CollectSET = unique(df.("Collect Tactic"));
     DriveSET = unique(df.("Drive Tactic"));
-    
-    if Scenario == "S0"
-        subsetDF = df(:,[Intent 13:15]);
-    else
-        subsetIndex = find(df.Scenario == Scenario);
-        subsetDF = df(subsetIndex,[Intent 13:15]);
-    end 
     
     for COLLECT = 1:length(unique(subsetDF.("Collect Tactic")))
         for DRIVE = 1:length(unique(subsetDF.("Drive Tactic")))
@@ -26,10 +26,10 @@ function output = IntelligentDECISION(df, Intent, Scenario)
     end
     
     switch Intent 
-        case {1 4 5 7 9 11} % sort = descend
+        case {1 4 5 7 9 11} % intent = maximise; sort = descend
             SortedValues = reshape(sort(TacticMatrix(:), 'descend'), [size(TacticMatrix,1)*size(TacticMatrix,2), 1]); % sort values 
             SortedUnique = sort(unique(SortedValues), 'descend'); % linear indices
-        case {2 3 6 8} % Intent = decision change --> sort = ascend
+        case {2 3 6 8} % intent = minimise; sort = ascend
             SortedValues = reshape(sort(TacticMatrix(:), 'ascend'), [size(TacticMatrix,1)*size(TacticMatrix,2), 1]);
             SortedUnique = sort(unique(SortedValues), 'ascend'); 
     end 
@@ -43,8 +43,8 @@ function output = IntelligentDECISION(df, Intent, Scenario)
     
     TacticSet.SortedValues = SortedValues; % values in sorted order for the intent
     TacticSet.SortedUnique = SortedUnique; % unique values in sorted order for the intent
-    TacticSet.SortedIdx = [r c];
-    TacticSet.SortedIndices = SortedIndices; 
+    TacticSet.SortedIndices = SortedIndices; % sorted tacitc-pair linear indices 
+    TacticSet.SortedIdx = [r c]; % COLLECT and DRIVE indices for tactic-pairs
     
     output.TacticMatrix = TacticMatrix; 
     output.TacticSet = TacticSet; 
