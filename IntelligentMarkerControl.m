340126da909931afc9b55d0ef3b1490d1939d5ce
fadamos <hepworth.adam@gmail.com>
2022-09-23 20:28:45 +1000

    classifiers built

--- IntelligentMarkerControl.m
+++ IntelligentMarkerControl.m
@@ -2,4 +2,9 @@ function output = IntelligentMarkerContro(df, Context, parameters)
     % Author: Adam J Hepworth
     % LastModified: 2022-07-22
     % Explanaton: Intelligent control agent
+
+    %% C1: Agent Classification (Type = 7 class)
+
+    %% C2: Swarm Classification (Type = binary)
+
 end
