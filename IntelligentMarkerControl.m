ecafed6828d4922f22f849a9e5aaba7dc8443faf
fadamos <hepworth.adam@gmail.com>
2022-09-23 20:46:49 +1000

    Update IntelligentMarkerControl.m

--- IntelligentMarkerControl.m
+++ IntelligentMarkerControl.m
@@ -1,10 +1,28 @@
-function output = IntelligentMarkerContro(df, Context, parameters)
+function output = IntelligentMarkerContro(df, Context, parameters, intent, behaviourLibrary, TP)
     % Author: Adam J Hepworth
-    % LastModified: 2022-07-22
+    % LastModified: 2022-09-23
     % Explanaton: Intelligent control agent
+
+    %% This is the flow chart as a very simple agent in the first instance
+    
 
     %% C1: Agent Classification (Type = 7 class)
 
     %% C2: Swarm Classification (Type = binary)
+
+    %% if Heterogeneous == C2_He 
+
+    %% if Homogeneous == C2_Ho 
+
+    %% if number of swarm classification sequences > threshold || swarm 2-class type > XXX threshold
+
+        %% Query library for optimal set of behaviours 
+    
+        %% Are suitable for intent? 
+    
+        %% Select optimal (based on optimal mean TP now, not just from the available) 
+
+        %% if predicted sequence of TP > threshold2 (i.e. invariant of selection) 
+            % change TP 
 
 end
